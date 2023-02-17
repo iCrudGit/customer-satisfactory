c0b296ecfb3b3fbfd2731805c40d9fef39ec495c
iCrudGit <zomi143icrud@gmail.com>
2023-02-17 10:25:07 +0800

    Create the header component

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ import { ToastContainer } from 'react-toastify'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home';
 import Header from './components/Header';
+import Login from './pages/Login';
 
 function App() {
   return (
     <div className="App">
       <ToastContainer />
-      <Header />
       <BrowserRouter>
+      <Header />
         <Routes>
-          <Route path="/" element={<Home />}/>
+          <Route path="/home" element={<Home />}/>
+          <Route path="/login" element={<Login />}/>
         </Routes>
       </BrowserRouter>
     </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,52 @@
-import React from 'react'
-import logo from '../images/logo.png'
+import React from "react";
+import logo from "../images/logo.png";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const Header = () => {
-  return (
-    <header>
-        <h1>Header</h1>
-        <img src={logo} alt="" width='100px' />
-    </header>
-  )
-}
+  const location = useLocation()
+  const navigate = useNavigate()
 
-export default Header
+  function pathMathRoute(route) {
+    if (route === location.pathname) {
+      return true;
+    }
+  }
+
+  return (
+    <div className="bg-white border-b shadow-sm sticky top-0 z-50">
+      <header className="flex justify-between items-center px-3 max-w-6xl mx-auto">
+        <div>
+          <img
+            src={logo}
+            alt=""
+            width="100px"
+            className="h-12 w-12 cursor-pointer"
+            onClick={()=>navigate("/")}
+          />
+        </div>
+        <div>
+          <ul className="flex space-x-10">
+            <li
+              className={`text-gray-400 font-semibold cursor-pointer py-3 border-b-[3px] border-b-transparent ${
+                pathMathRoute("/") && "text-black border-b-red-600"
+              }`}
+              onClick={()=>navigate("/")}
+            >
+              Home
+            </li>
+            <li
+              className={`text-gray-400 font-semibold cursor-pointer py-3 border-b-[3px] border-b-transparent ${
+                pathMathRoute("/login") && "text-black border-b-red-600"
+              }`}
+              onClick={()=>navigate("/login")}
+            >
+              Sign-In
+            </li>
+          </ul>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
